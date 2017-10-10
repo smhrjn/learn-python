6281d61466e68f04913e3a275534abcd3791dcde
smhrjn <sarat.maharjan@gmail.com>
2017-10-10 23:20:29 +0200

    gui tkinter

---
+++ gui-tk.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root = Tk()
+my_label = Label(root, text = 'I am l label')
+my_button = Button(root, text = 'I am a button')
+my_label.pack()
+my_button.pack()
+root.mainloop()
